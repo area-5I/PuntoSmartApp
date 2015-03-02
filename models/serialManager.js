9891531cdd889b97b13634147c597088a6dce51d
sass3r <mrsblast@gmail.com>
2015-03-01 22:41:10 -0400

    [EDIT] serialManager.js

--- models/serialManager.js
+++ models/serialManager.js
@@ -34,20 +34,19 @@ var colgar = function(){
 
 function initEstadoDeLlamada(){
     var interval = setInterval(function () {
+      datos = "";
     	gsmEntel.write("at+clcc \r\n");
     	console.log(datos);
     	var index = datos.indexOf("+CLCC:");
     	var estado = datos.charAt(index+11);
-        console.log(index);
-	console.log(estado);
-	if(estado == '0'){
-          datos = "";
+      console.log(index);
+	    console.log(estado);
+	    if(estado == '0'){
           //datos = datos.substring(index+6,datos.length);
-	  clearInterval(interval);
-	  console.log("llamada contestada");
-    	  server.socketio.sockets.emit("LlamadaContestada");
-	}
-	datos = "";
+	        clearInterval(interval);
+	        console.log("llamada contestada");
+    	    server.socketio.sockets.emit("LlamadaContestada");
+	    }
     	//datos = datos.substring(index+6,datos.length);
     },350);
 
